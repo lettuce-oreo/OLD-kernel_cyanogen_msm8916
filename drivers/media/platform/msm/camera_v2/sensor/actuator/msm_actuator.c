6c2dce6b8fc435cb5426b798123ae5fd33785ea8
Michael Bestas <mkbestas@lineageos.org>
2018-02-26 19:52:26 +0200

    Merge remote-tracking branch 'msm8916/lineage-15.1' into lineage-15.1
    [ghostrider-reborn]:
    * aargh so many conflicts! I should've done this earlier

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -94,6 +94,9 @@ static void msm_actuator_parse_i2c_params(struct msm_actuator_ctrl_t *a_ctrl,
 		(!a_ctrl->reg_tbl) ||
 		(!a_ctrl->i2c_reg_tbl)) {
 		pr_err("failed. NULL actuator pointers");
+
+	if (a_ctrl->i2c_reg_tbl == NULL) {
+		pr_err("failed. i2c reg tabl is NULL");
 		return;
 	}
 
