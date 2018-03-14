342900ca10d31e65f7dc2f489a188374dcece8b1
Ad!thya R <adithya.r02@outlook.com>
2018-03-14 12:55:58 +0530

    sched: Add missing declarations and fix build
    * Add missing declaration for sched_update_freq_max_load
    
    * Turn inline int sched_set_window into a static int coz
      we aren't using sched_set_window inline anymore.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1809,8 +1809,11 @@ extern unsigned long sched_get_busy(int cpu);
 extern void sched_get_cpus_busy(struct sched_load *busy,
 				const struct cpumask *query_cpus);
 extern void sched_set_io_is_busy(int val);
+int sched_update_freq_max_load(const cpumask_t *cpumask);
 #else
-static inline int sched_set_window(u64 window_start, unsigned int window_size)
+static int sched_set_window(u64 window_start, unsigned int window_size);
+
+static inline int sched_update_freq_max_load(const cpumask_t *cpumask)
 {
 	return -EINVAL;
 }
